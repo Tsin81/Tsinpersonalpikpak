81db86be4720c5184e331ff617a2b101bc572191
Tsin81 <82496344+Tsin81@users.noreply.github.com>
2023-09-01 22:08:15 +0800

    更改图标测试

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,11 +1,13 @@
 export const proxy = [
-  'https://diii.tk',
-  'https://api-pikpak.tjsky.cf',
-  'https://api-pikpak.tjsky-1.cf',
-  'https://cc123.cf',
-  'https://cf-api.xswd.cf',
-  'https://wkr.btstream.net',
-  'https://pikpak.520936.xyz',
+  'https://proxy.tsin.site',
+  'https://pikpak.tsin.site'
+  // 'https://diii.tk',
+  // 'https://api-pikpak.tjsky.cf',
+  // 'https://api-pikpak.tjsky-1.cf',
+  // 'https://cc123.cf',
+  // 'https://cf-api.xswd.cf',
+  // 'https://wkr.btstream.net',
+  // 'https://pikpak.520936.xyz',
 ]
 
 export const version = '1.25.5'
